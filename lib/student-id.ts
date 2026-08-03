bfc097fc461f357ba55ce2384b5e7aacb3d06e97
Madison <madisondixon.ai@gmail.com>
2026-08-03 18:32:41 -0400

    notes

--- lib/student-id.ts
+++ lib/student-id.ts
@@ -4,4 +4,4 @@
 // 👉 Paste your assigned user ID between the quotes
 // You do NOT need to change this when copying a new version into app/page.tsx
 
-export const userId = "";
+export const userId = "d5c3f040-e191-42f6-b869-894c4fe47210";
